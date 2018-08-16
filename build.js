7e3360d1ccde3742ab4433d60b110f21b1c65b2c
Alexis Tessier <alexistessierdev@gmail.com>
2018-08-16 14:11:14 +0200

    start adding tests

---
+++ build.js
@@ -0,0 +1,10 @@
+console.log('to do: build')
+
+const DEPENDENCIES_LIST = [
+	'hard-rejection@1.0.0',
+	'registry-auth-token@3.3.1',
+	'inquirer@3.1.1',
+	'shelljs@0.7.8',
+	'glob@7.1.2',
+	'jsonlint@1.6.2'
+].join(' ')
